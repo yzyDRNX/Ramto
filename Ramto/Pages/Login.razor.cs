1338817978f22be0bed8b4650c722622a452e6f6
Bboywil97 <131089907+Bboywil97@users.noreply.github.com>
2025-02-21 14:16:37 -0600

    login

---
+++ Ramto/Pages/Login.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Ramto.Lib.Locator;
+using Ramto.Lib.ViewModels;
+using Microsoft.JSInterop;
+
+namespace Ramto.Pages
+{
+    public partial class Login
+    {
+        [Inject]
+        private IJSRuntime JS { get; set; }
+
+        public LoginViewModel viewModel = Locator.GetViewModel<LoginViewModel>();
+
+    }
+}
--- Ramto/Program.cs
+++ Ramto/Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Ramto;
+using Ramto.Lib.OS;
+using Ramto.Modelos.Custom;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+using var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+
+
+// Se lee el archivo Json del que se obtienen valores 
+SettingsValuesClient.CurrentApiValues = await httpClient.GetFromJsonAsync<ApiValues>("settings.json");
 
 await builder.Build().RunAsync();
